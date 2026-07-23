48e77fb9c81ca1468308a6ce7804a8724373341e
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-23 10:54:44 +0000

    feat(niveau2): GREEN - implementation du taux d'endettement (seuil 33%)

--- src/main/java/com/creditengine/CreditEligibilityEngine.java
+++ src/main/java/com/creditengine/CreditEligibilityEngine.java
@@ -3,11 +3,19 @@ package com.creditengine;
 public class CreditEligibilityEngine {
 
     private static final int RESTE_A_VIVRE_MINIMUM = 100_000;
+    private static final double TAUX_ENDETTEMENT_MAXIMUM = 33.0;
 
     public EligibilityDecision evaluate(LoanApplication application) {
         int resteAVivre = application.monthlyIncome() - application.monthlyExpenses();
 
         if (resteAVivre < RESTE_A_VIVRE_MINIMUM) {
+            return EligibilityDecision.REJETE;
+        }
+
+        int mensualite = application.requestedAmount() / application.durationInMonths();
+        double tauxEndettement = ((application.monthlyExpenses() + mensualite) * 100.0) / application.monthlyIncome();
+
+        if (tauxEndettement > TAUX_ENDETTEMENT_MAXIMUM) {
             return EligibilityDecision.REJETE;
         }
 
--- src/test/java/com/creditengine/CreditEligibilityEngineTest.java
+++ src/test/java/com/creditengine/CreditEligibilityEngineTest.java
@@ -19,7 +19,8 @@ class CreditEligibilityEngineTest {
 
     @Test
     void approuve_quand_le_reste_a_vivre_est_exactement_100000() {
-        LoanApplication application = new LoanApplication("client-2", 200_000, 100_000, 500_000, 12);
+        // revenus=140000, charges=40000 => reste=100000, taux=(40000+583)/140000*100=28.99% (<33%, non bloquant)
+        LoanApplication application = new LoanApplication("client-2", 140_000, 40_000, 7_000, 12);
 
         EligibilityDecision decision = engine.evaluate(application);
 
@@ -28,7 +29,8 @@ class CreditEligibilityEngineTest {
 
     @Test
     void approuve_quand_le_reste_a_vivre_est_superieur_a_100000() {
-        LoanApplication application = new LoanApplication("client-3", 300_000, 100_000, 500_000, 12);
+        // revenus=400000, charges=100000 => reste=300000, taux=(100000+1000)/400000*100=25.25% (<33%, non bloquant)
+        LoanApplication application = new LoanApplication("client-3", 400_000, 100_000, 12_000, 12);
 
         EligibilityDecision decision = engine.evaluate(application);
 
