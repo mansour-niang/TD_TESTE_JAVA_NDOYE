1a68acd52fdd6782ec3b97a205045db291dc2254
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-23 10:57:40 +0000

    refactor(niveau4): extraction de methodes privees (resteAVivre, mensualite, taux, plafond)

--- src/main/java/com/creditengine/CreditEligibilityEngine.java
+++ src/main/java/com/creditengine/CreditEligibilityEngine.java
@@ -7,23 +7,33 @@ public class CreditEligibilityEngine {
     private static final double TAUX_ENDETTEMENT_MAXIMUM_FONCTIONNAIRE = 40.0;
 
     public EligibilityDecision evaluate(LoanApplication application) {
-        int resteAVivre = application.monthlyIncome() - application.monthlyExpenses();
-
-        if (resteAVivre < RESTE_A_VIVRE_MINIMUM) {
+        if (resteAVivre(application) < RESTE_A_VIVRE_MINIMUM) {
             return EligibilityDecision.REJETE;
         }
 
-        int mensualite = application.requestedAmount() / application.durationInMonths();
-        double tauxEndettement = ((application.monthlyExpenses() + mensualite) * 100.0) / application.monthlyIncome();
-
-        double plafondTauxEndettement = application.isCivilServant()
-                ? TAUX_ENDETTEMENT_MAXIMUM_FONCTIONNAIRE
-                : TAUX_ENDETTEMENT_MAXIMUM_STANDARD;
-
-        if (tauxEndettement > plafondTauxEndettement) {
+        if (tauxEndettement(application) > plafondTauxEndettement(application)) {
             return EligibilityDecision.REJETE;
         }
 
         return EligibilityDecision.APPROUVE;
+    }
+
+    private int resteAVivre(LoanApplication application) {
+        return application.monthlyIncome() - application.monthlyExpenses();
+    }
+
+    private int mensualiteEstimee(LoanApplication application) {
+        return application.requestedAmount() / application.durationInMonths();
+    }
+
+    private double tauxEndettement(LoanApplication application) {
+        int chargesTotales = application.monthlyExpenses() + mensualiteEstimee(application);
+        return (chargesTotales * 100.0) / application.monthlyIncome();
+    }
+
+    private double plafondTauxEndettement(LoanApplication application) {
+        return application.isCivilServant()
+                ? TAUX_ENDETTEMENT_MAXIMUM_FONCTIONNAIRE
+                : TAUX_ENDETTEMENT_MAXIMUM_STANDARD;
     }
 }
