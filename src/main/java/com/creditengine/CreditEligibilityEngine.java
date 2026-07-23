282305f5943e9f67e503b19d6fef89629fdcf9ad
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-23 10:56:45 +0000

    feat(niveau3): GREEN - plafond 40% pour les fonctionnaires

--- src/main/java/com/creditengine/CreditEligibilityEngine.java
+++ src/main/java/com/creditengine/CreditEligibilityEngine.java
@@ -3,7 +3,8 @@ package com.creditengine;
 public class CreditEligibilityEngine {
 
     private static final int RESTE_A_VIVRE_MINIMUM = 100_000;
-    private static final double TAUX_ENDETTEMENT_MAXIMUM = 33.0;
+    private static final double TAUX_ENDETTEMENT_MAXIMUM_STANDARD = 33.0;
+    private static final double TAUX_ENDETTEMENT_MAXIMUM_FONCTIONNAIRE = 40.0;
 
     public EligibilityDecision evaluate(LoanApplication application) {
         int resteAVivre = application.monthlyIncome() - application.monthlyExpenses();
@@ -15,7 +16,11 @@ public class CreditEligibilityEngine {
         int mensualite = application.requestedAmount() / application.durationInMonths();
         double tauxEndettement = ((application.monthlyExpenses() + mensualite) * 100.0) / application.monthlyIncome();
 
-        if (tauxEndettement > TAUX_ENDETTEMENT_MAXIMUM) {
+        double plafondTauxEndettement = application.isCivilServant()
+                ? TAUX_ENDETTEMENT_MAXIMUM_FONCTIONNAIRE
+                : TAUX_ENDETTEMENT_MAXIMUM_STANDARD;
+
+        if (tauxEndettement > plafondTauxEndettement) {
             return EligibilityDecision.REJETE;
         }
 
