a4fbb8f1204aff48a2ec515032b76931019575e7
mansour-niang <mansour.niang4@unchk.edu.sn>
2026-07-23 10:49:28 +0000

    test(niveau2): RED - regle du taux d'endettement (seuil 33%)

--- src/test/java/com/creditengine/CreditEligibilityEngineTest.java
+++ src/test/java/com/creditengine/CreditEligibilityEngineTest.java
@@ -34,4 +34,24 @@ class CreditEligibilityEngineTest {
 
         assertEquals(EligibilityDecision.APPROUVE, decision);
     }
+
+    @Test
+    void rejette_quand_le_taux_d_endettement_depasse_33_pourcent() {
+        // revenus=300000, charges=90000, mensualite=120000/12=10000 => taux=(90000+10000)/300000*100=33.33%
+        LoanApplication application = new LoanApplication("client-4", 300_000, 90_000, 120_000, 12);
+
+        EligibilityDecision decision = engine.evaluate(application);
+
+        assertEquals(EligibilityDecision.REJETE, decision);
+    }
+
+    @Test
+    void approuve_quand_le_taux_d_endettement_est_exactement_33_pourcent() {
+        // revenus=300000, charges=89000, mensualite=120000/12=10000 => taux=(89000+10000)/300000*100=33.0%
+        LoanApplication application = new LoanApplication("client-5", 300_000, 89_000, 120_000, 12);
+
+        EligibilityDecision decision = engine.evaluate(application);
+
+        assertEquals(EligibilityDecision.APPROUVE, decision);
+    }
 }
